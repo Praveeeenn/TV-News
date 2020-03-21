88166d77dfa2b5fa07f130eba575d88038da22f4
Praveen <praveenislive@gmail.com>
2020-03-21 12:02:34 +0530

    Initial Commit

---
+++ TV News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TV News
+//
+//  Created by Praveen on 21/03/20.
+//  Copyright © 2020 Praveen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
